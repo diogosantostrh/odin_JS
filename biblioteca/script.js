6933c344ff3695652801d5574c20bdba33dcce55
Diogo SAntos <diogo.santos@techremotehub.com>
2022-04-04 09:53:46 +0100

    Update script.js

--- biblioteca/script.js
+++ biblioteca/script.js
@@ -11,9 +11,23 @@ function adicionar() {
         this.paginas=paginas
     }
 
-    const livro = new Livro(nome.value,autor.value,paginas.value);
+    function addlivro(nome,autor,paginas) {
+        const livro = new Livro(nome,autor,paginas)
+        /*book.push([livro.nome,livro.autor,livro.paginas])*/
 
-    res.innerHTML='Nome:' + livro.nome+'<br>'
-    res.innerHTML+='Autor:' + livro.autor+'<br>'
-    res.innerHTML+= 'Paginas:' + livro.paginas+'<br>'
+        res.innerHTML='Nome:' + livro.nome+'<br>'
+        res.innerHTML+='Autor:' + livro.autor+'<br>'
+        res.innerHTML+= 'Paginas:' + livro.paginas+'<br>'
+    }
+    if (book.length == 0) {
+        addlivro(nome.value,autor.value,paginas.value)
+        book.push([nome.value,autor.value,paginas.value])
+    } else {
+        addlivro(nome.value,autor.value,paginas.value)
+    }
+    
+    console.log(book)
+    console.log(book.length)
+    
+    
 }
